f51256511fd14669f289ea4fbdbb509d50a18697
Jonathan <Razordor@gmail.com>
2022-08-10 20:36:03 -0700

    reduced compile-time
    Replaced `windows` with `windows-sys` dependency, so compile-time is reduced to nothing.
    Got rid of abstractions that doesn't add anything of value.

--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,7 @@ pub use dylink_macro::dylink;
 // perpetuate dependency for macro
 #[doc(hidden)]
 pub use once_cell::sync::Lazy;
-use windows::{
-	core,
+use windows_sys::{
 	Win32::{
 		Foundation::HINSTANCE,
 		Graphics::OpenGL as gl,
@@ -32,46 +31,22 @@ use windows::{
 type DllHandle = Mutex<UnsafeCell<Vec<(String, HINSTANCE)>>>;
 static DLL_DATA: Lazy<DllHandle> = Lazy::new(|| Mutex::new(UnsafeCell::new(Vec::new())));
 
-#[derive(Clone, Copy)]
-#[repr(transparent)]
-pub struct DispatchableHandle(pub *const ffi::c_void);
+pub type DispatchableHandle = *const ffi::c_void;
 
-/// Context is used in place of `VkInstance` to invoke the vulkan specialization.
-struct Context {
-	instance: AtomicPtr<ffi::c_void>,
-	device:   AtomicPtr<ffi::c_void>,
+pub struct VkContext {
+	pub instance: AtomicPtr<ffi::c_void>,
+	pub device:   AtomicPtr<ffi::c_void>,
 }
 
-/// Setting instance allows dylink to load Vulkan functions.
-#[inline]
-pub fn set_instance(inst: DispatchableHandle) {
-	CONTEXT.instance.store(inst.0 as *mut _, Ordering::Release);
-}
-
-/// Setting device to a non-null value lets Dylink call `vkGetDeviceProcAddr`.    
-#[inline]
-pub fn set_device(dev: DispatchableHandle) {
-	CONTEXT.device.store(dev.0 as *mut _, Ordering::Release);
-}
-
-#[inline]
-pub fn get_instance() -> DispatchableHandle {
-	DispatchableHandle(CONTEXT.instance.load(Ordering::Acquire))
-}
-
-#[inline]
-pub fn get_device() -> DispatchableHandle {
-	DispatchableHandle(CONTEXT.device.load(Ordering::Acquire))
-}
-
-static CONTEXT: Context = Context {
+/// `VK_CONTEXT` is loaded every time `vkloader` is called.
+pub static VK_CONTEXT: VkContext = VkContext {
 	instance: AtomicPtr::new(ptr::null_mut()),
 	device:   AtomicPtr::new(ptr::null_mut()),
 };
 
 /// `vkloader` is a vulkan loader specialization.
-pub fn vkloader(fn_name: &str) -> Option<fn()> {
-	// let context = context.borrow();
+#[track_caller]
+pub fn vkloader(fn_name: &str) -> fn() {	
 	#[allow(non_snake_case)]
 	#[allow(non_upper_case_globals)]
 	static vkGetInstanceProcAddr: Lazy<
@@ -83,15 +58,16 @@ pub fn vkloader(fn_name: &str) -> Option<fn()> {
 		extern "stdcall" fn(DispatchableHandle, *const c_char) -> *const ffi::c_void,
 	> = Lazy::new(|| unsafe {
 		std::mem::transmute(vkGetInstanceProcAddr(
-			get_instance(),
+			VK_CONTEXT.instance.load(Ordering::Acquire),
 			"vkGetDeviceProcAddr\0".as_ptr() as *const c_char,
 		))
 	});
 
 	let c_fn_name = ffi::CString::new(fn_name).unwrap();
-	let device = get_device();
-	let addr = if device.0.is_null() {
-		vkGetInstanceProcAddr(get_instance(), c_fn_name.as_ptr())
+	let device = VK_CONTEXT.device.load(Ordering::Acquire);
+	let instance = VK_CONTEXT.instance.load(Ordering::Acquire);
+	let addr = if device.is_null() {
+		vkGetInstanceProcAddr(instance, c_fn_name.as_ptr())
 	} else {
 		let addr = vkGetDeviceProcAddr(device, c_fn_name.as_ptr());
 		if addr.is_null() {
@@ -100,30 +76,31 @@ pub fn vkloader(fn_name: &str) -> Option<fn()> {
 				"Dylink Warning: `{fn_name}` not found using `vkGetDeviceProcAddr`. Deferring \
 				 call to `vkGetInstanceProcAddr`."
 			);
-			vkGetInstanceProcAddr(get_instance(), c_fn_name.as_ptr())
+			vkGetInstanceProcAddr(instance, c_fn_name.as_ptr())
 		} else {
 			addr
 		}
-	};
-	if addr.is_null() {
-		None
-	} else {
-		unsafe { Some(mem::transmute(addr)) }
+	};	
+	assert!(!addr.is_null());
+	unsafe {
+		mem::transmute(addr)
 	}
 }
 
 /// `glloader` is an opengl loader specialization.
-pub fn glloader(fn_name: &str) -> *const ffi::c_void {
+#[track_caller]
+pub fn glloader(fn_name: &str) -> fn() {
 	unsafe {
 		let c_fn_name = ffi::CString::new(fn_name).unwrap();
-		let addr = gl::wglGetProcAddress(core::PCSTR(c_fn_name.as_ptr() as *const _))
-			.expect(&format!("Dylink Error: `{fn_name}` not found!"));
+		let addr = gl::wglGetProcAddress(c_fn_name.as_ptr() as *const _)
+			.expect(&format!("Dylink Error: `{fn_name}` not found!"));			
 		mem::transmute(addr)
 	}
 }
 
 /// `loader` is a generalization for all other dlls.
-pub fn loader(lib_name: &str, fn_name: &str) -> ptr::NonNull<ffi::c_void> {
+#[track_caller]
+pub fn loader(lib_name: &str, fn_name: &str) -> fn() {
 	let mut lib_handle = HINSTANCE::default();
 	let mut lib_found = false;
 	unsafe {
@@ -137,8 +114,8 @@ pub fn loader(lib_name: &str, fn_name: &str) -> ptr::NonNull<ffi::c_void> {
 
 		if !lib_found {
 			let lib_cstr = ffi::CString::new(lib_name).unwrap();
-			lib_handle = LoadLibraryA(core::PCSTR(lib_cstr.as_ptr() as *const _))
-				.expect("Dylink Error: `{}` not found!");
+			lib_handle = LoadLibraryA(lib_cstr.as_ptr() as *const _);
+			assert!(lib_handle != 0, "Dylink Error: `{lib_name}` not found!");
 			(*dll_data).push((lib_name.to_string(), lib_handle));
 		}
 	}
@@ -148,8 +125,11 @@ pub fn loader(lib_name: &str, fn_name: &str) -> ptr::NonNull<ffi::c_void> {
 	let addr: *const ffi::c_void = unsafe {
 		std::mem::transmute(GetProcAddress(
 			lib_handle,
-			core::PCSTR(fn_cstr.as_ptr() as *const _),
+			fn_cstr.as_ptr() as *const _,
 		))
 	};
-	ptr::NonNull::new(addr as *mut _).expect(&format!("Dylink Error: `{fn_name}` not found!"))
+	assert!(!addr.is_null(), "Dylink Error: `{fn_name}` not found!");
+	unsafe {
+		mem::transmute(addr)
+	}
 }
