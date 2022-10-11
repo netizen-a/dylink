2e6918e0aedc1a9d6665318d3743ffc487cbae8a
Jonathan <Razordor@gmail.com>
2022-10-11 00:02:47 -0700

    simplified some code
    Replaced legacy system calls with more modern calls.
    Also, removed macros since rust can support sys calling conventions natively using `extern "system"`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 // Copyright (c) 2022 Jonathan "Razordor" Alan Thomason
 #![feature(strict_provenance)]
+#![warn(fuzzy_provenance_casts)]
 
 // re-export the dylink macro
 pub extern crate dylink_macro;
@@ -16,95 +17,12 @@ use std::{
 
 pub use dylink_macro::dylink;
 use once_cell::sync::Lazy;
-use windows_sys::Win32::{
-	Foundation::{HINSTANCE, PROC},
-	Graphics::OpenGL as gl,
-	System::LibraryLoader::{GetProcAddress, LoadLibraryA},
-};
-
-#[macro_export]
-macro_rules! vk_platform {
-    (
-        $(#[$attr:meta])+
-        extern VKAPI_CALL {
-            $($token:tt)+
-        }
-    ) => {
-        #[cfg(windows)]
-        vk_platform!{
-            $(#[$attr])+
-            extern "stdcall" {
-                $($token)+
-            }
-        }
-        #[cfg(not(windows))]
-        vk_platform!{
-            $(#[$attr])+
-            extern "C" {
-                $($token)+
-            }
-        }
-    };
-    (
-        $(#[$attr:meta])+
-        extern $conv:literal {
-            $($token:tt)+
-        }
-    ) => {
-        $(#[$attr])+
-        extern $conv {
-            $($token)+
-        }
-    };
-    (
-        $(
-            $(#[$attr:meta])*
-            $visibility:vis type $id:ident = extern VKAPI_CALL fn (
-                $($token:tt)*
-            ) $(-> $ret:ty)?;
-        )+
-    ) => {
-		#[cfg(windows)]
-		vk_platform!{
-			$(
-			    $(#[$attr])*
-			    $visibility type $id = extern "stdcall" fn (
-			        $($token)*
-			    ) $(-> $ret)?;
-			)+
-		}
-		#[cfg(not(windows))]
-		vk_platform!{
-			$(
-			    $(#[$attr])*
-			    $visibility type $id = extern "C" fn (
-			        $($token)*
-			    ) $(-> $ret)?;
-			)+
-		}
-    };
-	(
-        $(
-            $(#[$attr:meta])*
-            $visibility:vis type $id:ident = extern $conv:literal fn (
-                $($token:tt)*
-            ) $(-> $ret:ty)?;
-        )+
-    ) => {
-        $(
-            $(#[$attr])*
-            $visibility type $id = extern $conv fn (
-                $($token)*
-            ) $(-> $ret)?;
-        )+
-    };
-}
 
 type DispatchableHandle = *const ffi::c_void;
-vk_platform! {
-	#[allow(non_camel_case_types)]
-	type PFN_vkGetProcAddr = extern VKAPI_CALL fn(DispatchableHandle, *const c_char) -> Option<fn()>;
-}
+
+#[allow(non_camel_case_types)]
+type PFN_vkGetProcAddr = extern "system" fn(DispatchableHandle, *const c_char) -> Option<fn()>;
+
 
 // This is pretty much impossible to use safely without the dylink macro
 #[repr(transparent)]
@@ -146,7 +64,7 @@ pub static VK_CONTEXT: VkContext = VkContext {
 /// # Panics
 /// This function might panic if `vulkan-1.dll` is not found or if the function is not found.
 #[track_caller]
-pub unsafe fn vkloader(fn_name: &str) -> Option<fn()> {
+pub unsafe fn vkloader(fn_name: &str) -> Option<unsafe extern "system" fn()> {
 	let device = VK_CONTEXT.device.load(Ordering::Acquire);
 	let instance = VK_CONTEXT.instance.load(Ordering::Acquire);
 	#[allow(non_upper_case_globals)]
@@ -167,34 +85,41 @@ pub unsafe fn vkloader(fn_name: &str) -> Option<fn()> {
 		vkGetDeviceProcAddr(device, c_fn_name.as_ptr())
 			.or_else(|| vkGetInstanceProcAddr(instance, c_fn_name.as_ptr()))
 	};
-	addr
+	mem::transmute(addr)
 }
 
 /// `glloader` is an opengl loader specialization.
 /// # Panics
 /// This function might panic if the function is not found.
 #[track_caller]
-pub unsafe fn glloader(fn_name: &str) -> PROC {
+pub unsafe fn glloader(fn_name: &str) -> Option<unsafe extern "system" fn()> {
+	use windows_sys::Win32::Graphics::OpenGL::wglGetProcAddress;
 	let c_fn_name = ffi::CString::new(fn_name).unwrap();
-	let addr = gl::wglGetProcAddress(c_fn_name.as_ptr() as *const _);
-	addr
+	let addr = wglGetProcAddress(c_fn_name.as_ptr() as *const _);
+	mem::transmute(addr)
 }
 
 /// `loader` is a generalization for all other dlls.
 /// # Panics
 /// This function might panic if the `lib_name` dll is not found or if the function is not found.
 #[track_caller]
-pub unsafe fn loader(lib_name: &str, fn_name: &str) -> Option<fn()> {
+pub unsafe fn loader(lib_name: &str, fn_name: &str) -> Option<unsafe fn()> {
+	use windows_sys::Win32::{
+		System::LibraryLoader::{GetProcAddress, LoadLibraryExA, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS},
+		Foundation::HINSTANCE,
+	};	
 	use std::collections::HashMap;
+
 	static DLL_DATA: Lazy<RwLock<HashMap<String, HINSTANCE>>> =
 		Lazy::new(|| RwLock::new(HashMap::new()));
+
 	let read_lock = DLL_DATA.read().unwrap();
 	let handle: HINSTANCE = match read_lock.get(lib_name) {
 		Some(lib_handle) => *lib_handle,
 		None => {
 			mem::drop(read_lock);
 			let lib_cstr = ffi::CString::new(lib_name).unwrap();
-			let lib_handle = LoadLibraryA(lib_cstr.as_ptr() as *const _);
+			let lib_handle = LoadLibraryExA(lib_cstr.as_ptr() as *const _, 0, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
 			assert!(lib_handle != 0, "Dylink Error: `{lib_name}` not found!");
 			DLL_DATA
 				.write()
