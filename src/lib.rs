25b66a5895c8345c22d3aa8bb6faf1a319c442f2
Jonathan <Razordor@gmail.com>
2022-10-28 17:50:33 -0700

    updated docs

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@ use std::{ffi, ptr, sync::atomic::AtomicPtr};
 pub mod error;
 pub mod example;
 pub mod lazyfn;
+
+// TODO: make this work through more than just windows
+#[cfg(windows)]
 pub mod loader;
 
 pub struct VkContext {
@@ -14,15 +17,16 @@ pub struct VkContext {
 	pub device:   AtomicPtr<ffi::c_void>,
 }
 
-// TODO: hide VK_CONTEXT behind a trait
 
-/// `VK_CONTEXT` is loaded every time `vkloader` is called.
+/// This global is read every time a vulkan function is called for the first time,
+/// which silently occurs through `LazyFn::link_lib`.
 pub static VK_CONTEXT: VkContext = VkContext {
 	instance: AtomicPtr::new(ptr::null_mut()),
 	device:   AtomicPtr::new(ptr::null_mut()),
 };
 
-/// Used as a placeholder function pointer
+/// Used as a placeholder function pointer. This should **NEVER** be called directly, 
+/// and promptly cast into the correct function pointer type.
 pub type FnPtr = unsafe extern "system" fn() -> isize;
 /// The result of a Dylink function
 pub type Result<T> = std::result::Result<T, crate::error::DylinkError>;
