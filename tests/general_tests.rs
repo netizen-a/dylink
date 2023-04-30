3be088ec930fee22f2d8086bcf3b6c600529bc74
Jonathan <Razordor@gmail.com>
2023-04-30 02:38:09 -0700

    leveraged traits
    These changes will make it no longer backwards compatible with `0.2.1`
    
    used `PathBuf` where useful

--- tests/general_tests.rs
+++ tests/general_tests.rs
@@ -19,16 +19,13 @@ fn test_win32_kernel32() {
 fn test_win32_lifetimes() {
 	use dylink::LazyFn;
 	use std::sync::atomic::AtomicPtr;
-	use std::ffi::CStr;
 
 	extern "stdcall" fn foo() -> u32 { 0 }
 	type PfnTy = extern "stdcall" fn () -> u32;
 
-	const FN_NAME: &'static CStr = unsafe {CStr::from_bytes_with_nul_unchecked(b"SetLastError\0")};
-
 	let lazyfn = LazyFn::<PfnTy>::new(AtomicPtr::new(&mut (foo as PfnTy)));
 	let old_ref = lazyfn.as_ref();
-	let new_addr = lazyfn.load(FN_NAME, dylink::LinkType::System(&["Kernel32.dll"])).unwrap();
+	let new_addr = lazyfn.load("SetLastError", dylink::LinkType::System(&["Kernel32.dll"])).unwrap();
 	
 	assert_eq!(*old_ref, foo as PfnTy);	
 	assert_ne!(new_addr, foo as PfnTy);	
