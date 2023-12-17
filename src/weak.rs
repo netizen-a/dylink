410137669ab43dfbe9921414b17b59c41d0f3fa6
Jonathan <Razordor@gmail.com>
2023-12-16 19:49:06 -0800

    added Weak::new

--- src/weak.rs
+++ src/weak.rs
@@ -2,6 +2,7 @@ use crate::img;
 use crate::os;
 use crate::Library;
 use std::path;
+use std::ptr;
 
 #[cfg(unix)]
 use os::unix as imp;
@@ -19,6 +20,24 @@ pub struct Weak {
 impl crate::sealed::Sealed for Weak {}
 
 impl Weak {
+	/// Constructs a new `Weak`, without allocating any memory. Calling [`upgrade`] on the return value always gives [`None`].
+	///
+    /// [`upgrade`]: Weak::upgrade
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use dylink::Weak;
+    ///
+    /// let empty: Weak = Weak::new();
+    /// assert!(empty.upgrade().is_none());
+    /// ```
+	pub const fn new() -> Self {
+		Self {
+			base_addr: ptr::null(),
+			path_name: None,
+		}
+	}
 
 	// The `Library::this` instance doesn't work well in doc tests.
 
